3df609c3ca2fae8fd3db6821de2c5a3999c5f3a8
DabaniCode <dabani.rm@gmail.com>
2020-07-10 17:33:50 +0200

    Logging information

--- src/Hash.kt
+++ src/Hash.kt
@@ -1,62 +1,7 @@
 import com.rsk.security.Argument.argument
+import com.rsk.security.logger
 import java.io.*
 import java.security.MessageDigest
-import java.util.*
-
-abstract class SecurityBase(protected val outputStrategy: OutputStrategy) {
-    abstract fun run()
-
-    private val overwrite: Boolean by argument()
-    private val encode: Boolean by argument()
-
-    @Throws(IOException::class)
-    fun createOutputStream(fileName: String): OutputStream {
-        return if (fileName.isEmpty())
-            System.out
-        else {
-            val file = File(fileName)
-
-            val fileOutputStream = if (file.exists()) {
-                if (overwrite)
-                    FileOutputStream(file)
-                else
-                    throw IOException("Destination file already exists")
-            } else {
-                FileOutputStream(file)
-            }
-            fileOutputStream
-        }
-    }
-
-    @Throws(FileNotFoundException::class)
-    fun createInputStream(fileName: String): InputStream {
-        return if (fileName.isEmpty())
-            System.`in`
-        else {
-            val f = File(fileName)
-            if (f.exists()) {
-                FileInputStream(f)
-            } else {
-                throw FileNotFoundException()
-            }
-        }
-    }
-
-
-    @Throws(IOException::class)
-    fun writeBytes(os: OutputStream, bytes: ByteArray) {
-        if (encode) {
-            val temp: String
-            val encoder = Base64.getEncoder()
-            temp = encoder.encodeToString(bytes)
-            os.write(temp.toByteArray())
-        } else {
-            os.write(bytes)
-        }
-        os.flush()
-    }
-}
-
 
 class Hash (outputStrategy: OutputStrategy) : SecurityBase(outputStrategy) {
 
@@ -64,6 +9,8 @@ class Hash (outputStrategy: OutputStrategy) : SecurityBase(outputStrategy) {
     private val fileName: String by argument()
     private val destFileName: String by argument()
     private val provider: String  by argument()
+
+    private val logger by logger()
 
     class Help {
         fun help() {
